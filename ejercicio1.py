3a322eece6af9004a8acf581dc03b20041bc49c8
flay15 <fraydavidlopezfranco@gmail.com>
2024-09-18 09:48:34 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+# Operaciones con tres números enteros
+num1 = int(input("Ingresa el primer número entero: "))
+num2 = int(input("Ingresa el segundo número entero: "))
+num3 = int(input("Ingresa el tercer número entero: "))
+
+suma = num1 + num2 + num3
+resta = num1 - num3
+multiplicacion = num2 * num3
+division = num1 / num3
+
+print(f"Suma de los tres números: {suma}")
+print(f"Resta del primero menos el tercero: {resta}")
+print(f"Multiplicación del segundo por el tercero: {multiplicacion}")
+print(f"División del primero entre el tercero: {division}")
+
